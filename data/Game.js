8af33ea6cce4027a9409b38589add19feb23dcf7
Heark <adtune6509@student.waynecc.edu>
2016-09-14 14:21:56 -0400

    Update Game.js

--- data/Game.js
+++ data/Game.js
@@ -5,8 +5,11 @@ var game_started = false;
 $(document).ready(function() {
     if (game_started == false) {
         $("body").append("<button id=\"#Start\"> Start </button>");
-        game_started = true;
+        
     } else {
       // Do nothing...
     }
+    $("#Start").click(function(){
+        game_started = true;
+    })
 })
