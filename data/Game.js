eb6854e9c8d2da6f339232fd8f37f62542224529
Heark <adtune6509@student.waynecc.edu>
2016-09-14 14:18:51 -0400

    Update Game.js

--- data/Game.js
+++ data/Game.js
@@ -1,1 +1,12 @@
-{}
+// #Boolean Variables
+var game_started = false;
+
+
+$(document).ready(function() {
+    if (game_started == false) {
+        $("body").append("<button id=\"#Start\"> Start </button>");
+        game_started = true;
+    } else {
+      // Do nothing...
+    }
+})
