703338ca4fb1d2962a707c4ea0f4ba2ffb1a3993
Heark <adtune6509@student.waynecc.edu>
2016-11-21 14:39:01 -0500

    Update Items.js

--- data/Items.js
+++ data/Items.js
@@ -1,1 +1,6 @@
 // # Items Go Here
+// NAME, DESC, EFF
+
+var weak_potion = new CLASSES.Item("Weak Potion", "Heal yourself by 10 pts", 10);
+
+var goo = new CLASSES.Item("Goo", "Permanently Increase ATT by 1." 1, false);
