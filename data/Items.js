0fc354d739640d02488dd637db7daf664321abb5
Heark <adtune6509@student.waynecc.edu>
2016-11-21 14:39:28 -0500

    Update Items.js

--- data/Items.js
+++ data/Items.js
@@ -1,6 +1,6 @@
 // # Items Go Here
-// NAME, DESC, EFF
+// NAME, DESC, EFF, USINB
 
 var weak_potion = new CLASSES.Item("Weak Potion", "Heal yourself by 10 pts", 10);
 
-var goo = new CLASSES.Item("Goo", "Permanently Increase ATT by 1." 1, false);
+var goo = new CLASSES.Item("Goo", "Permanently Increase ATT by 1.", 1, false);
