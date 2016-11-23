ea09d164fe83df270fba573e10cfd4dfb7bdcd50
Heark <adtune6509@student.waynecc.edu>
2016-11-23 11:10:47 -0500

    Update Game.js

--- data/Game.js
+++ data/Game.js
@@ -3,6 +3,7 @@ console.log("Loaded Game.")
 var game_started = false;
 
 
+
 $(document).ready(function() {
     if (game_started == false) {
         $("body").append("<center><button id=\"Start\" class=\"btn btn-primary\"> Start </button></center>");
@@ -10,13 +11,24 @@ $(document).ready(function() {
     } else {
       // Do nothing...
     }
+    
+    // Start Button
     $("#Start").click(function(){
         if(game_started == false) {
-            $("body").append( "<center><button id=\"Walk\"  class=\"btn btn-primary\" > Walk </button></center>" );
+            $("body").append( "<center><button id=\"Walk\"  class=\"btn btn-success\" > Walk </button></center>" );
          game_started = true;
         } else {
             box.error("You already started!");
         }
         
     })
+    // End of Start Button
+    
+    // Walk Button
+        $("#Walk").click(function(){
+        
+        
+        })
+    // End of Walk Button
+    
 })
