d905d86f520d2b8a2d596c770bf7f9badd4e1098
Heark <adtune6509@student.waynecc.edu>
2016-11-23 11:05:44 -0500

    Update enemydrops.js

--- data/enemydrops.js
+++ data/enemydrops.js
@@ -1,1 +1,2 @@
+console.log("Loaded Enemy Drops");
 mob_1 = [weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, weak_potion, goo];
