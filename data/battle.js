72f4e3aeea1068608d1f99b5a4eac8dd67c9ed1b
Heark <adtune6509@student.waynecc.edu>
2016-11-28 14:52:56 -0500

    Update battle.js

--- data/battle.js
+++ data/battle.js
@@ -4,7 +4,7 @@ var BATTLE_START = function() {
         var POSS_EM;
         if (Hero.LVL <= 5) {
             POSS_EM = [slime];
-        } else if () {}
+        } //else if () {}
 
         var sel_em = Tools.shuffle(POSS_EM);
 
