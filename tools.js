f4d6b7dd833a395a5556c06313d99131069218ba
Heark <adtune6509@student.waynecc.edu>
2016-11-28 14:20:23 -0500

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,6 +1,12 @@
 console.log("Loaded Tools");
 Tools = {
-
+shuffle: function(contents) {
+            var output;
+            this.contents = contents;
+            output = this.contents[Math.floor(Math.random() * this.contents.length)];
+            return output;
+},
+  
 randNum: function(min, max) {
   return Math.random() * (max - min) + min;
 }
