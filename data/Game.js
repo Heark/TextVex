6baa357e5f3b6a3f59380aed700f499a42d71e27
Heark <adtune6509@student.waynecc.edu>
2016-11-29 14:14:51 -0500

    Update Game.js

--- data/Game.js
+++ data/Game.js
@@ -1,5 +1,5 @@
 console.log("Loaded Game.")
-// #Boolean Variables
+    // #Boolean Variables
 var game_started = false;
 var gotNum;
 
@@ -8,34 +8,34 @@ var gotNum;
 $(document).ready(function() {
     if (game_started == false) {
         $("body").append("<center><button id=\"Start\" class=\"btn btn-primary\"> Start </button></center>");
-        
+
     } else {
-      // Do nothing...
+        // Do nothing...
     }
-    
+
     // Start Button
-    $("#Start").click(function(){
-        if(game_started == false) {
-            $("body").append( "<center><button id=\"Walk\"  class=\"btn btn-success\" > Walk </button></center>" );
-         game_started = true;
-        } else {
-            box.error("You already started!");
-        }
-        
-    })
-    // End of Start Button
-    
+    $("#Start").click(function() {
+            if (game_started == false) {
+                $("body").append("<center><button id=\"Walk\"  class=\"btn btn-success\" > Walk </button></center>");
+                game_started = true;
+            } else {
+                box.error("You already started!");
+            }
+
+        })
+        // End of Start Button
+
     // Walk Button
-        $("#Walk").click(function(){
+
+})
+$("#Walk").click(function() {
         gotNum = Tools.randNum(1, 200);
         console.log(gotNum)
-        if(gotNum <= 100 ){
-         // no battle   
-        } else if(gotNum > 200){
-        Scene.call.Battle();
+        if (gotNum <= 100) {
+            // no battle   
+        } else if (gotNum > 200) {
+            Scene.call.Battle();
         }
-        
-        })
+
+    })
     // End of Walk Button
-    
-})
