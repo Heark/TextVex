521e3ab4c9e0f913b90a9d592bcd50de252e6816
Heark <adtune6509@student.waynecc.edu>
2017-01-18 14:31:32 -0500

    Update scene.js

--- data/scene.js
+++ data/scene.js
@@ -69,13 +69,15 @@ function clearInterface() {
     callback: function (result) {
         console.log('This was logged in the callback: ' + result);
     }
-                }
+                })
 
 
-                if (enemy_turn == true) {}
+                if(enemy_turn == true) {}
 
             }
 
         }
     }
+}
+
 }
