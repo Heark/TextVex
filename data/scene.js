02ef3dab2c2cd2b4d90aa5ae38564b30911af63a
Heark <adtune6509@student.waynecc.edu>
2017-01-18 14:42:18 -0500

    Update scene.js

--- data/scene.js
+++ data/scene.js
@@ -1,23 +1,29 @@
+
 var Scene = {
-        Call: {
+    Call: {
 
-        Battle:  function() {
+        Battle: function(Enemy) {
+        var sel_em;
+            // Select Random enemy
+            function randEn() {
+            var POSS_EM;
+            if (Hero.LVL <= 5) {
+                POSS_EM = [slime];
+            } //else if () {}
 
-        // Select Random enemy
-        var POSS_EM;
-        if (Hero.LVL <= 5) {
-            POSS_EM = [slime];
-        } //else if () {}
-
-        var sel_em = Tools.shuffle(POSS_EM);
-
-        // End of enemy selection
-
-        // start of battle
-        var player_turn;
-        var enemy_turn;
-        battle_begin = function() {
-            bootbox.confirm({
+            sel_em = Tools.shuffle(POSS_EM);
+            }
+            // End of enemy selection
+           if(this.Enemy == null || undefined){
+                   randEn();
+           } else {
+                   sel_em = this.Enemy;
+           }
+            // start of battle
+            var player_turn;
+            var enemy_turn;
+            battle_begin = function() {
+                bootbox.confirm({
                     message: "A " + sel_em.NAME + " appeared, battle it?",
                     buttons: {
                         confirm: {
@@ -40,44 +46,46 @@ var Scene = {
                             } else {}
                         }
                     }
-                    
-                    });
+
+                });
             }
-        battle_begin();
-function clearInterface() {
-        
-}
+            battle_begin();
+
+            function clearInterface() {
+
+            }
+
             function go() {
-                    player_turn = true;
+                player_turn = true;
                 if (player_turn == true) {
-                bootbox.dialog({
-    message: "What do you do?",
-    buttons: {
-        confirm: {
-            label: 'Yes',
-            className: 'btn-success'
-        },
-        cancel: {
-            label: 'No',
-            className: 'btn-danger'
-        }, 
-        test: {
-                label: 'test',
-                className: 'btn-danger'
-        }
-    },
-    callback: function (result) {
-        console.log('This was logged in the callback: ' + result);
-    }
-                })
+                    bootbox.dialog({
+                        message: "What do you do?",
+                        buttons: {
+                            fight: {
+                                label: 'Attack',
+                                className: 'btn-success'
+                            },
+                            bag: {
+                                label: 'Bag',
+                                className: 'btn-success'
+                            },
+                            flee: {
+                                label: 'Flee',
+                                className: 'btn-danger'
+                            }
+                        },
+                        callback: function(result) {
+                            console.log('This was logged in the callback: ' + result);
+                        }
+                    })
 
 
-                if(enemy_turn == true) {}
+                    if (enemy_turn == true) {}
+
+                }
 
             }
-
         }
     }
-}
 
 }
